fdd7d88d4cf72b71160f48d3de2c6d2e02498779
Khorasani95 <hafizmuhammadabdullah29@gmail.com>
2024-07-20 20:52:19 +0500

    Update header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,37 +1,15 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
 
-export default function Header() {
+function Header() {
   return (
     <>
-      <nav className="navbar navbar-expand-lg bg-body-tertiary">
-        <div className="container-fluid">
-          {/* <a className="navbar-brand" >Navbar</a> */}
-          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span className="navbar-toggler-icon"></span>
-          </button>
-          <div className="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-              <li className="nav-item">
-                <Link to={"/"} className="nav-link active" aria-current="page" >Home</Link>
-              </li>
-              <li className="nav-item">
-                <Link to={"/login"} className="nav-link" >Login</Link>
-              </li>
-              <li className="nav-item">
-                <Link to={"/register"} className="nav-link" >Register</Link>
-              </li>
-              <li className="nav-item">
-                <Link to={"/Forget"} className="nav-link" >Forget</Link>
-              </li>
-              <li className="nav-item">
-                <Link to={"/todoList"} className="nav-link" >Todo-List</Link>
-              </li>
-              
-            </ul>
-          </div>
+      <nav class="navbar bg-body-tertiary">
+        <div class="container-fluid">
+          <span class="navbar-brand mb-0 h1">Todo App</span>
         </div>
       </nav>
     </>
   )
 }
+
+export default Header;
